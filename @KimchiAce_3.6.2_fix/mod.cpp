ae653fbdb965081565701be2c96343c6fa81c92c
honorlglory <honorlglory@naver.com>
2017-02-17 17:55:31 +0900

    초기화
    깃허브 초기화

--- @KimchiAce_3.6.2_fix/mod.cpp
+++
@@ -1,8 +0,0 @@
-name = "Kimchiguys Ace Addons 3.6.2v Fixed";
-picture = "mod.paa";
-actionName = "Website";
-action = "http://cafe.naver.com/kimchiguys";
-overview = "Kimchiguys Main addon package";
-overviewPicture = "mod.paa";
-overviewText = "Kimchiguys Ace Addons 3.6.2v Fixed";
-overviewFootnote = "<br /><br /><t color='#999999'>This content is under ACE3MOD Team License.<t />";
